5e73ed557fc4f79187428585b3c040c310282a67
Xose Pérez <xose.perez@gmail.com>
2023-09-17 00:38:20 +0200

    Added envvars for INTERFACE, RESET_GPIO, ENABLE_GPIO and HAS_GPS

--- chirpstack-concentratord-docker/start.sh
+++ chirpstack-concentratord-docker/start.sh
@@ -105,6 +105,53 @@ if [[ "$DESIGN" == "sx1301" ]]; then
 fi
 
 # -----------------------------------------------------------------------------
+# Identify interface
+# -----------------------------------------------------------------------------
+
+# Models with USB interface
+MODELS_WITH_USB="RAK7271 RAK7371 RAK5148 SX1280ZXXXXGW1"
+if [[ $MODELS_WITH_USB =~ (^|[[:space:]])${ORIGINAL_MODEL^^}($|[[:space:]]) ]]; then
+    INTERFACE="${INTERFACE:-"USB"}"
+else
+    INTERFACE=${INTERFACE:-"SPI"}
+fi
+
+# -----------------------------------------------------------------------------
+# GPIO configuration
+# -----------------------------------------------------------------------------
+
+# If interface is USB disable RESET_GPIO, otherwise default to 17
+if [[ "$INTERFACE" == "SPI" ]]; then
+    RESET_GPIO=${RESET_GPIO:-17}
+else
+    RESET_GPIO=${RESET_GPIO:-0}
+fi
+
+# The RAK833-SPI/USB has a SPDT to select USB/SPI interfaces
+# If used with RAK2247 or RAK2287 hats this is wired to GPIO20
+if [[ "${ORIGINAL_MODEL^^}" == "RAK833" ]]; then
+    if [[ "$INTERFACE" == "SPI" ]]; then
+        POWER_EN_GPIO=${POWER_EN_GPIO:-20}
+        POWER_EN_LOGIC=${POWER_EN_LOGIC:-0}
+    fi
+fi
+
+# Otherwise the default is no power enable pin
+POWER_EN_GPIO=${POWER_EN_GPIO:-0}
+POWER_EN_LOGIC=${POWER_EN_LOGIC:-1}
+
+# -----------------------------------------------------------------------------
+# GPS Configuration
+# -----------------------------------------------------------------------------
+
+# Models with GPS
+MODELS_WITH_GPS="RAK7243 RAK7243C RAK7244 RAK7244C RAK7246G RAK7248 RAK7248C RAK831 RAK2245"
+if [[ $MODELS_WITH_GPS =~ (^|[[:space:]])${ORIGINAL_MODEL^^}($|[[:space:]]) ]]; then
+    HAS_GPS=${HAS_GPS:-1}
+fi
+HAS_GPS=${HAS_GPS:-0}
+
+# -----------------------------------------------------------------------------
 # Debug
 # -----------------------------------------------------------------------------
 
@@ -116,11 +163,17 @@ else
 echo -e "${COLOR_INFO}Model:          $MODEL${COLOR_END}"
 fi
 echo -e "${COLOR_INFO}Design:         ${DESIGN}${COLOR_END}"
+echo -e "${COLOR_INFO}Interface:      $INTERFACE${COLOR_END}"
+if [[ "$INTERFACE" == "SPI" ]]; then
+echo -e "${COLOR_INFO}Reset GPIO:     $RESET_GPIO${COLOR_END}"
+echo -e "${COLOR_INFO}Enable GPIO:    $POWER_EN_GPIO${COLOR_END}"
+fi
 if [[ "$DESIGN" == "sx1301" ]]; then
 echo -e "${COLOR_INFO}Gateway EUI:    $GATEWAY_EUI${COLOR_END}"
 fi
 echo -e "${COLOR_INFO}Region:         $REGION${COLOR_END}"
 echo -e "${COLOR_INFO}Channels:       $CHANNELS${COLOR_END}"
+echo -e "${COLOR_INFO}Has GPS:        $HAS_GPS${COLOR_END}"
 
 echo -e "${COLOR_INFO}------------------------------------------------------------------${COLOR_END}"
 
@@ -133,6 +186,12 @@ cp ./artifacts/configs/chirpstack-concentratord-${DESIGN}/concentratord.toml  ./
 cp ./artifacts/configs/chirpstack-concentratord-${DESIGN}/region_${REGION}.toml ./region.toml
 cp ./artifacts/configs/chirpstack-concentratord-${DESIGN}/channels_${CHANNELS}.toml ./channels.toml
 
+# Calculate model flags
+MODEL_FLAGS=""
+if [[ "$INTERFACE" == "USB" ]]; then MODEL_FLAGS="$MODEL_FLAGS USB"; fi
+if [[ $HAS_GPS -eq 1 ]]; then MODEL_FLAGS="$MODEL_FLAGS GNSS"; fi
+MODEL_FLAGS=$( echo ${MODEL_FLAGS##*( )} | sed 's/^/["/' | sed 's/$/"]/' | sed 's/ /","/' )
+
 # modify the settings based on environment variables
 sed "s/model=\".*\"/model=\"${MODEL}\"/" -i concentratord.toml 
 sed "s/region=\".*\"/region=\"${REGION^^}\"/" -i concentratord.toml 
@@ -142,6 +201,18 @@ sed "s/log_to_syslog=.*/log_to_syslog=false/" -i concentratord.toml
 if [ "$SOCKET_PREFIX" != "" ]; then
     sed "s/_bind=\"ipc:\/\/\/tmp\/concentratord_/_bind=\"ipc:\/\/\/tmp\/concentratord_${SOCKET_PREFIX}_/" -i concentratord.toml 
 fi
+
+if [[ "$DESIGN" == "sx1301" ]]; then
+    sed "s/model_flags=.*/model_flags=${MODEL_FLAGS}/" -i concentratord.toml 
+    sed "s/reset_pin=.*/reset_pin=${RESET_GPIO^^}/" -i concentratord.toml 
+fi
+
+if [[ "$DESIGN" == "sx1302" ]]; then
+    sed "s/model_flags=.*/model_flags=${MODEL_FLAGS}/" -i concentratord.toml 
+    if [[ $RESET_GPIO -ne 0 ]]; then echo -e "\nsx1302_reset_pin=${RESET_GPIO^^}" >> concentratord.toml ; fi
+    if [[ $POWER_EN_GPIO -ne 0 ]]; then echo -e "\nsx1302_power_en_pin=${POWER_EN_GPIO^^}" >> concentratord.toml ; fi
+fi
+
 
 # -----------------------------------------------------------------------------
 # Get binary
@@ -153,4 +224,5 @@ mv chirpstack-concentratord-${DESIGN} chirpstack-concentratord
 # -----------------------------------------------------------------------------
 # Start concentratord
 # -----------------------------------------------------------------------------
-./chirpstack-concentratord -c ./concentratord.toml -c ./region.toml -c ./channels.toml
+#./chirpstack-concentratord -c ./concentratord.toml -c ./region.toml -c ./channels.toml
+cat ./concentratord.toml
