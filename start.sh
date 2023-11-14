d296c0a5828bb237e29be9b8968f0661aa26674d
Xose Pérez <xose.perez@gmail.com>
2023-11-14 17:20:02 +0100

    Pass BAND and NETID on variables

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 export ARCH=arm64
-export MODEL=RAK5148
-export BAND=ism2400
-export CHIRPSTACK_GATEWAY_BRIDGE_HOSTS=127.0.0.1,localhost
-export MQTT_BROKER_HOSTS=127.0.0.1,localhost
+export MODEL=RAK5146
+export BAND=eu868
+export CHIRPSTACK_GATEWAY_BRIDGE_HOSTS=127.0.0.1,localhost,192.168.42.137
+export MQTT_BROKER_HOSTS=127.0.0.1,localhost,192.168.42.137
 export API_SECRET=e0A0dkoGQxqdX8R4g3l2XYg/uFGHZy+VGtyov6juszo=
 
-docker compose -f docker-compose.yml -f docker-compose-concentratord.yml -f docker-compose-bridge-concentratord.yml up -d
+docker compose -p chirpstack -f docker-compose.yml -f docker-compose-bridge-udp.yml -f docker-compose-bridge-basicstation.yml up -d
+#docker compose -p chirpstack -f docker-compose.yml -f docker-compose-concentratord.yml -f docker-compose-mqtt-forwarder.yml up -d
+#docker compose -p chirpstack  -f docker-compose.yml -f docker-compose-concentratord.yml -f docker-compose-bridge-concentratord.yml up -d
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml -f docker-compose-concentratord.yml -f docker-compose-bridge-concentratord.yml down
+docker compose -p chirpstack down
