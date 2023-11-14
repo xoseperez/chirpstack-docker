4a9cc0b4d9e50902830ee0b57ffc4864dd0f8cce
Xose Pérez <xose.perez@gmail.com>
2023-11-15 00:50:43 +0100

    Merge concentratord and concentratord-bridge in a single YAML

--- start.sh
+++ start.sh
@@ -7,13 +7,14 @@ export CHIRPSTACK_GATEWAY_BRIDGE_HOSTS=127.0.0.1,localhost
 export MQTT_BROKER_HOSTS=127.0.0.1,localhost
 export API_SECRET=e0A0dkoGQxqdX8R4g3l2XYg/uFGHZy+VGtyov6juszo=
 export NET_ID=000000
+export USER_ID=$( id -u )
+export GROUP_ID=$( id -g )
 
 declare SERVICES=(
     chirpstack
     bridge-udp
     bridge-basicstation 
-    #concentratord
-    #bridge-concentratord
+    concentratord
     #mqtt-forwarder
 )
 
