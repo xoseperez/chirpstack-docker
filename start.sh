1c32457d04343bc313ff3b453cf8d6ad17d4684a
Xose Pérez <xose.perez@gmail.com>
2023-11-16 15:24:41 +0100

    Redo certfiicates ENV variables

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ export ARCH=arm64
 export MODEL=RAK5146
 export BAND=eu868
 export HOSTS=127.0.0.1,localhost
+export CHIRPSTACK_GATEWAY_BRIDGE_HOSTS=$HOSTS
+export MQTT_BROKER_HOSTS=$HOSTS
 export API_SECRET=e0A0dkoGQxqdX8R4g3l2XYg/uFGHZy+VGtyov6juszo=
 export NET_ID=000000
 export USER_ID=$( id -u )
