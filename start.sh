b9c69d9afcc457cb4cd3ed694c3312b5763bce9f
Xose Pérez <xose.perez@gmail.com>
2024-03-14 21:56:36 +0100

    Manage PUID and PGID, clean up

--- start.sh
+++ start.sh
@@ -8,8 +8,11 @@ export CHIRPSTACK_GATEWAY_BRIDGE_HOSTS=$HOSTS
 export MQTT_BROKER_HOSTS=$HOSTS
 export API_SECRET=e0A0dkoGQxqdX8R4g3l2XYg/uFGHZy+VGtyov6juszo=
 export NET_ID=000000
-export USER_ID=$( id -u )
-export GROUP_ID=$( id -g )
+export PUID=$( id -u )
+export PGID=$( id -g )
+export PGID_GPIO=$( grep ^gpio /etc/group | cut -d':' -f3 )
+export PGID_I2C=$( grep ^i2c /etc/group | cut -d':' -f3 )
+export PGID_SPI=$(  grep ^spi /etc/group | cut -d':' -f3 )
 
 declare SERVICES=(
     chirpstack
@@ -20,8 +23,8 @@ declare SERVICES=(
 )
 
 FILES=""
-for i in ${!SERVICES[@]}; do
+for i in "${!SERVICES[@]}"; do
     FILES="$FILES -f docker-compose-${SERVICES[i]}.yml "
 done
 
-docker compose -p chirpstack $FILES up -d
+docker compose -p chirpstack "$FILES" up -d
