035f54687cdf35faf48b42ba03e6acec6beb8a53
Xose Pérez <xose.perez@gmail.com>
2025-03-18 21:33:06 +0100

    Add PGAdmin, fix mosquitto config path

--- configuration/postgresql/initdb/001-init-chirpstack.sh
+++ configuration/postgresql/initdb/001-init-chirpstack.sh
@@ -4,4 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     create role chirpstack with login password 'chirpstack';
     create database chirpstack with owner chirpstack;
+    create database chirpstack_fuota with owner chirpstack;
 EOSQL
--- configuration/postgresql/initdb/002-chirpstack_extensions.sh
+++ configuration/postgresql/initdb/002-chirpstack_extensions.sh
@@ -5,3 +5,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="chirpstack" <<-EOS
     create extension pg_trgm;
     create extension hstore;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="chirpstack_fuota" <<-EOSQL
+    create extension hstore;
+EOSQL
